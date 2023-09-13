526000b22e3bd3ed2d88da082d97aa26b6f0bbce
KhalidFinny <katthoriq@gmail.com>
2023-09-13 12:02:32 +0700

    Updated terminal login

--- Fitur/Terminallogin.java
+++ Fitur/Terminallogin.java
@@ -6,24 +6,36 @@ public class Terminallogin {
         Scanner scanner = new Scanner(System.in);
 
         // Predefined login credentials
-        String username = "Admin";
-        String password = "tifast!";
+        String correctUsername = "Admin";
+        String correctPassword = "tifast!";
 
-        System.out.println("Welcome to Sistem Tata Tertib");
+        System.out.println("Welcome to Sistem Tata Tertib Mahasiswa");
 
-        // Ask for username and password
-        System.out.print("Enter username: ");
-        String enteredUsername = scanner.nextLine();
-        System.out.print("Enter password: ");
-        String enteredPassword = scanner.nextLine();
+        boolean isLoggedIn = false;
 
-        // Check if the entered credentials match
-        if (enteredUsername.equals(username) && enteredPassword.equals(password)) {
-            System.out.println("Login successful. Welcome, " + username + "!");
-        } else {
-            System.out.println("Login failed. Invalid username or password.");
-        }
+        do {
+            // Ask for username and password
+            System.out.print("Enter username: ");
+            String enteredUsername = scanner.nextLine();
+            System.out.print("Enter password: ");
+            String enteredPassword = scanner.nextLine();
 
+            // Check if the entered credentials match
+            if (enteredUsername.equals(correctUsername) && enteredPassword.equals(correctPassword)) {
+                System.out.println("Login successful. Welcome, " + correctUsername + "!");
+                isLoggedIn = true;
+            } else {
+                System.out.println("Login failed. Invalid username or password. Please try again.");
+            }
+        } while (!isLoggedIn);
+        String nama, kelas;
+        System.out.print("Masukkan nama:");
+        nama = scanner.nextLine();
+        System.out.print("masukkan kelas:");
+        kelas = scanner.nextLine();
+        System.out.println("Nama anda:"+nama);
+        System.out.println("kelas anda:"+kelas);
         scanner.close();
+        
     }
 }
