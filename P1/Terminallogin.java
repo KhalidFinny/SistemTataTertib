d7a87d7539df3c94c2c342cee79345ed38217370
KhalidFinny <katthoriq@gmail.com>
2023-09-13 11:44:28 +0700

    Terminal login

---
+++ P1/Terminallogin.java
@@ -0,0 +1,29 @@
+package P1;
+import java.util.Scanner;
+
+public class Terminallogin {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Predefined login credentials
+        String username = "Admin";
+        String password = "tifast!";
+
+        System.out.println("Welcome to Sistem Tata Tertib");
+
+        // Ask for username and password
+        System.out.print("Enter username: ");
+        String enteredUsername = scanner.nextLine();
+        System.out.print("Enter password: ");
+        String enteredPassword = scanner.nextLine();
+
+        // Check if the entered credentials match
+        if (enteredUsername.equals(username) && enteredPassword.equals(password)) {
+            System.out.println("Login successful. Welcome, " + username + "!");
+        } else {
+            System.out.println("Login failed. Invalid username or password.");
+        }
+
+        scanner.close();
+    }
+}
