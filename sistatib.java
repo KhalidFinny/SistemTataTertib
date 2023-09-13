bbdc7c9db9d66b44a806dec282bc1ad46102642a
Maeeveee <142636445+Maeeveee@users.noreply.github.com>
2023-09-13 11:28:30 +0700

    login tatib

--- sistatib.java
+++ sistatib.java
@@ -1,11 +1,23 @@
 package SistemTataTertib;
+
+import java.util.Scanner;
+
 /**
  * sistatib
  */
 public class sistatib {
 
     public static void main(String[] args) {
-        System.out.println("Data pelanggaran mahasiswa");
-        
+        Scanner scanner =new Scanner(System.in);
+        String nama, kelas;
+
+        System.out.print("Masukkan nama:");
+        nama = scanner.nextLine();
+        System.out.print("masukkan kelas:");
+        kelas = scanner.nextLine();
+        System.out.println("Nama anda:"+nama);
+        System.out.printaku("kelas anda:"+kelas);
+        scanner.close();//oke
+    
     }
 }
