97d1875a120c2948d60cc93a59f2d97453671322
Maeeveee <142636445+Maeeveee@users.noreply.github.com>
2023-09-13 08:16:50 +0700

    input nama kelas

---
+++ sistatib.java
@@ -0,0 +1,11 @@
+package SistemTataTertib;
+/**
+ * sistatib
+ */
+public class sistatib {
+
+    public static void main(String[] args) {
+        System.out.println("Data pelanggaran mahasiswa");
+        
+    }
+}
